01e0f4b6140ffc891a423b871364a39e8899c9fc
Adam Phillips <aphillips@scholastic.co.uk>
2018-04-19 22:33:20 +0100

    - add initial integration test

---
+++ test/system/admin/create_questionnaire_test.rb
@@ -0,0 +1,13 @@
+require 'application_system_test_case'
+
+class CreateQuestionnaireTest < ApplicationSystemTestCase
+  it 'should be possible to create a questionnaire' do
+    visit '/admin'
+    click_link 'Add new'
+    fill_in 'Name', with: 'Questionnaire 1'
+    click_button 'Save'
+
+    assert page.has_content?('Questionnaire created')
+    assert page.hash_content?('Questionnaire 1')
+  end
+end
