33b8a9300cb05d930aa3439df811a351f915a7a4
Adam Phillips <aphillips@scholastic.co.uk>
2018-04-21 23:15:49 +0100

    - refine app initialisation

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -10,19 +10,16 @@ import store from '../app/store';
 import Admin from '../screens/Admin';
 import AddQuestionnaire from '../screens/AddQuestionnaire';
 
-import createHistory from 'history/createBrowserHistory';
-const history = createHistory();
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Provider store={store}>
-      <Router history={history} basename="/admin">
+      <Router basename="/admin">
         <div>
           <Route exact path="/" component={Admin} />
           <Route path="/add-questionnaire" component={AddQuestionnaire} />
         </div>
       </Router>
     </Provider>,
-    document.body.appendChild(document.createElement('div'))
+    document.body.appendChild(document.getElementById('admin-app'))
   );
 });
