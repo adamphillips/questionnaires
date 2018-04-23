96e6b0d46576731bf513d2ab505c299aff7b27f1
Adam Phillips <aphillips@scholastic.co.uk>
2018-04-23 12:24:48 +0100

    - remove unneeded model name behaviour

--- app/domain/form.rb
+++ app/domain/form.rb
@@ -2,21 +2,14 @@
 class Form
   extend ActiveModel::Naming
   include ActiveModel::Conversion
-
-  def self.model_name
-    name = @model_name_override || default_model_name
-    ActiveModel::Name.new(self, nil, name)
-  end
-
-  def self.set_model_name model_name
-    @model_name_override = model_name
-  end
+  include ActiveModel::Validations
 
   def persisted?; false; end
 
-  private
-
-  def self.default_model_name
-    name.to_s.demodulize.sub(/Form$/, '')
+  def attributes= attrs
+    return unless attrs.present?
+    for key, value in attrs
+      send :"#{key}=", value
+    end
   end
 end
