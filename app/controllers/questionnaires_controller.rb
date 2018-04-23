d9d9f8b082a274a8c9902d30b4054c54192b447b
Adam Phillips <aphillips@scholastic.co.uk>
2018-04-23 12:28:58 +0100

    -update controller to submit form

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -6,6 +6,20 @@ class QuestionnairesController < ApplicationController
   def show
   end
 
+  def submit
+    SubmitQuestionnaire
+      .new(questionnaire_form)
+      .call(questionnaire_response_params) do |response|
+      response.on(:success) do
+        redirect_to :thanks
+      end
+
+      response.on(:validation_error) do
+        render 'show'
+      end
+    end
+  end
+
   private
 
   def questionnaire_form
@@ -14,5 +28,13 @@ class QuestionnairesController < ApplicationController
 
   def questionnaire
     @questionnaire ||= Questionnaire.find(params[:id])
+  end
+
+  def questionnaire_response_params
+    @questionnaire_response_params ||= params.require(:questionnaire_form).permit(questionnaire_form_fields)
+  end
+
+  def questionnaire_form_fields
+    [:person_name] + questionnaire_form.questions.collect(&:form_field)
   end
 end
