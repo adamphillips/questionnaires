7aa5a11ad234887a256cc1f74ea6b6fe32320695
Adam Phillips <aphillips@scholastic.co.uk>
2018-04-23 23:12:44 +0100

    - vq screen proptypes

--- app/javascript/screens/ViewQuestionnaire.js
+++ app/javascript/screens/ViewQuestionnaire.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
 
@@ -11,6 +12,10 @@ const ViewQuestionnaire = (props) => {
       <QuestionnaireResponses id={Number(props.match.params.id)} />
     </div>
   );
+};
+
+ViewQuestionnaire.propTypes = {
+  match: PropTypes.object
 };
 
 export default ViewQuestionnaire;
