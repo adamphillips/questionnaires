8fc16708de9b07ed7a8f700e33c1d6f0927336c8
Adam Phillips <aphillips@scholastic.co.uk>
2018-04-23 10:35:30 +0100

    - add test for submitting qu

---
+++ test/system/complete_questionnaire_test.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'application_system_test_case'
+
+require 'support/factory_bot'
+
+class CompleteQuestionnaireTest < ApplicationSystemTestCase
+  describe 'as a user' do
+    it 'should be possible to complete a questionnaire' do
+      questionnaire = create :questionnaire, title: 'Test questionnaire', questions: [
+        { label: 'Question 1', name: 'question_1' },
+        { label: 'Question 2', name: 'question_2' }
+      ]
+
+      visit questionnaire_url(questionnaire)
+
+      fill_in 'Question 1', with: 'Answer 1'
+      fill_in 'Question 2', with: 'Answer 2'
+
+      click_button 'Submit'
+
+      assert page.has_content?('Thank you for submitting the questionnaire')
+    end
+  end
+end
