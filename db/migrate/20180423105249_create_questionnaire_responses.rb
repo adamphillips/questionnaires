56077e7c09430b54fa5bae88c1e42324aafee4cd
Adam Phillips <aphillips@scholastic.co.uk>
2018-04-23 12:24:27 +0100

    - update responses schema

--- db/migrate/20180423105249_create_questionnaire_responses.rb
+++ db/migrate/20180423105249_create_questionnaire_responses.rb
@@ -1,8 +1,11 @@
 class CreateQuestionnaireResponses < ActiveRecord::Migration[5.2]
   def change
     create_table :questionnaire_responses do |t|
+      t.references :questionnaire
       t.string :person_name
       t.json :answers
     end
+
+    add_foreign_key :questionnaire_responses, :questionnaires
   end
 end
