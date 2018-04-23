80f1690ed65495f14edda34d2af8d728b57a4427
Adam Phillips <aphillips@scholastic.co.uk>
2018-04-23 22:23:11 +0100

    - enable view questionnaire admin urls

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
-    get '(:path)' => 'home#index'
+    get '(:path)(/:id)' => 'home#index'
   end
 
   namespace :api do
